c45d7b88115362962ac2d20e5b15a6c17c34cc68
VikramB87 <vikram_b87@yahoo.com>
2014-01-27 14:30:46 +0530

    Minor changes

--- src/Test.scala
+++ src/Test.scala
@@ -1,8 +1,8 @@
 object Test extends App {
   
-  println (countChange(4, List(1,2), List()))
-  //println (countChange(50, List(1,2,5,10,20,50,100), List()))
-  //println (countChange(100, List(1,2,5,10,20,50,100), List()))
+  //println (countChange(4, List(1,2), List())) // 3
+  // println (countChange(50, List(1,2,5,10,20,50,100), List())) // 451
+  println (countChange(100, List(1,2,5,10,20,50,100), List()))
   
   def countChange(money: Int, coins: List[Int], usedCoins: List[Int]): Int = 
     if (money == 0) { /*println(usedCoins);*/ 1}
